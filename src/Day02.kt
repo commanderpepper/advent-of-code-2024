2800cb243ee59e25167ceef00ef056bc87d0936f
humza <ahmadhumza7@gmail.com>
2024-12-02 15:16:40 -0800

    Solved Day 2 part two

--- src/Day02.kt
+++ src/Day02.kt
@@ -6,11 +6,19 @@ fun main(){
     }
 
     fun part2(input:List<String>):Int {
-        return 0
+        val reports = getReports(input)
+        val sf = reports.map {
+            isReportSafe(report = it, operation = { a, b -> b - a })
+                    || isReportSafeWithDampener(report = it, operation = { a, b -> b - a })
+                    || isReportSafe(report = it, operation = { a, b -> a - b })
+                    || isReportSafeWithDampener(report = it, operation = { a, b -> a - b })
+        }
+        return sf.count { it }
     }
 
     val input = readInput("Day02")
     println(part1(input))
+    println(part2(input))
 }
 
 fun getReports(input:List<String>): List<List<Int>>{
@@ -22,4 +30,16 @@ fun getReports(input:List<String>): List<List<Int>>{
 fun isReportSafe(report: List<Int>, minAmount : Int = 1, maxAmount : Int = 3, operation : (Int, Int) -> (Int)): Boolean {
     val zipWithNext = report.zipWithNext { a, b -> operation(a, b) }
     return zipWithNext.all { it in minAmount ..  maxAmount }
+}
+
+fun isReportSafeWithDampener(report: List<Int>, minAmount : Int = 1, maxAmount : Int = 3, operation : (Int, Int) -> (Int)): Boolean {
+    val reportsWithOneItemDropped = mutableListOf<List<Int>>()
+    for(i in 0 until report.size){
+        val ml = report.toMutableList()
+        val r = ml.removeAt(i)
+        reportsWithOneItemDropped.add(ml.toList())
+    }
+    println(reportsWithOneItemDropped)
+    val areAnySafe = reportsWithOneItemDropped.any { isReportSafe(report = it, operation = operation) }
+    return areAnySafe
 }
