27328e536c7739c9e54e1c89ad1c24962c9339d1
humza <ahmadhumza7@gmail.com>
2024-12-05 13:00:48 -0800

    Solved Day 3 part two

--- src/Day03.kt
+++ src/Day03.kt
@@ -3,19 +3,28 @@ fun main(){
         return input.map{ line -> getPairsForMult(line).map { it.first * it.second }.sum() }.sum()
     }
 
-    fun part2(input:String):Int{
-        return 0
+    fun part2(input:List<String>):Int{
+        val valids = input.map { getValidString(it) }
+        return valids.map{ line -> getPairsForMult(line).map { it.first * it.second }.sum() }.sum()
     }
 
     val input = readInput("Day03")
     println(part1(input))
+    println(part2(input))
+}
+
+fun getValidString(line: String): String {
+    val regex = """(^|do\(\)).*?($|don't\(\))""".toRegex()
+    val joinToString = regex.findAll(line).map { it.value }.joinToString(separator = "")
+    println(joinToString)
+    return joinToString
 }
 
 fun getPairsForMult(input: String): List<Pair<Int,Int>>{
     val pairs = mutableListOf<Pair<Int,Int>>()
-    println(input)
     val mulRegex = "mul\\(\\d{1,3},\\d{1,3}\\)".toRegex()
     val numbersRegex = "\\d{1,3},\\d{1,3}".toRegex()
+
     val instructions = mulRegex.findAll(input).map { matchResult ->
         matchResult.value
     }
@@ -24,8 +33,5 @@ fun getPairsForMult(input: String): List<Pair<Int,Int>>{
     }
 
     numbers.filter { it.isNotEmpty() }.forEach { list -> pairs.add(Pair(list[0],list[1])) }
-
-    println(pairs)
-
     return pairs
 }
